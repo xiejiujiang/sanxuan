c44a79ae5cb2b7fcd892ef1073ce01862a11ede7
xiejiujiang <634482891@qq.com>
2022-12-12 17:35:04 +0800

    Initial commit

---
+++ src/main/java/com/example/sanxuan/SanxuanApplication.java
@@ -0,0 +1,13 @@
+package com.example.sanxuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanxuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanxuanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sanxuan/SanxuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanxuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanxuanApplicationTests {
+
+    /*@Test
+    void contextLoads() {
+    }*/
+
+}
